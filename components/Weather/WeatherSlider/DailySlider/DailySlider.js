1f19496e3f650ab4c1e0837c6bc45998325c9ed5
Saber Maram <s.maram@devcups.com>
2020-06-11 11:04:44 +0200

    fixing modals.

--- components/Weather/WeatherSlider/DailySlider/DailySlider.js
+++ components/Weather/WeatherSlider/DailySlider/DailySlider.js
@@ -29,7 +29,13 @@ const dailySlider = props => {
     }
   };
 
-  let checkHour = props.weather.hourly.data;
+  let checkHour = props.weather.hourly;
+
+  console.log(">>>>>>>>>>>>");
+  console.log(props.weather.hourly)
+  console.log(props.weather.hourly.data)
+
+
   const hourArray = () => {
     let newArray = checkHour.map(object => format(fromUnixTime(object.dt), "M/d/yyyy"));
     let oneDay = [...new Set(newArray)];
@@ -123,7 +129,7 @@ const dailySlider = props => {
                         <path d="M64.5 92.6c15.5 0 28-12.6 28-28s-12.6-28-28-28-28 12.6-28 28 12.5 28 28 28zm0-47.9c11 0 19.9 8.9 19.9 19.9 0 11-8.9 19.9-19.9 19.9s-19.9-8.9-19.9-19.9c0-11 8.9-19.9 19.9-19.9zM68.6 23.6V10.7c0-2.3-1.8-4.1-4.1-4.1s-4.1 1.8-4.1 4.1v12.9c0 2.3 1.8 4.1 4.1 4.1s4.1-1.8 4.1-4.1zM60.4 105.6v12.9c0 2.3 1.8 4.1 4.1 4.1s4.1-1.8 4.1-4.1v-12.9c0-2.3-1.8-4.1-4.1-4.1s-4.1 1.8-4.1 4.1zM96.4 38.5l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8 0l-9.1 9.1c-1.6 1.6-1.6 4.2 0 5.8.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2zM23.5 105.6c.8.8 1.8 1.2 2.9 1.2 1 0 2.1-.4 2.9-1.2l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8 0l-9.1 9.1c-1.6 1.6-1.6 4.2 0 5.8zM122.5 64.6c0-2.3-1.8-4.1-4.1-4.1h-12.9c-2.3 0-4.1 1.8-4.1 4.1s1.8 4.1 4.1 4.1h12.9c2.2 0 4.1-1.8 4.1-4.1zM10.6 68.7h12.9c2.3 0 4.1-1.8 4.1-4.1s-1.8-4.1-4.1-4.1H10.6c-2.3 0-4.1 1.8-4.1 4.1s1.9 4.1 4.1 4.1zM102.6 106.8c1 0 2.1-.4 2.9-1.2 1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8 0-1.6 1.6-1.6 4.2 0 5.8l9.1 9.1c.8.8 1.9 1.2 2.9 1.2zM38.4 38.5c1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8 0-1.6 1.6-1.6 4.2 0 5.8l9.1 9.1c.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2z" />
                       </svg>
                       <h4 className={styles["modal__item__title"]}>UV Index</h4>
-                      {item.uvIndex}
+                      {item.uvi}
                     </div>
                   </div>
                 </div>
@@ -161,11 +167,11 @@ const dailySlider = props => {
                       </li>
                       <li>
                         <strong>Sunrise</strong>{" "}
-                        <span>{format(fromUnixTime(item.sunriseTime), "h:m a")}</span>
+                        <span>{format(fromUnixTime(item.sunrise), "h:m a")}</span>
                       </li>
                       <li>
                         <strong>Sunset</strong>{" "}
-                        <span>{format(fromUnixTime(item.sunsetTime), "h:m a")}</span>
+                        <span>{format(fromUnixTime(item.sunset), "h:m a")}</span>
                       </li>
                       <li>
                         <strong>Moon Phase</strong>{" "}
@@ -198,7 +204,7 @@ const dailySlider = props => {
                       <li>
                         <strong>UV Index</strong>{" "}
                         <span>
-                          {item.uvIndex} at {format(fromUnixTime(item.uvIndexTime), "hh:mm a")}
+                          {item.uvi} at {format(fromUnixTime(item.uvi), "hh:mm a")}
                         </span>
                       </li>
                       <li>
@@ -231,13 +237,13 @@ const dailySlider = props => {
                               <tbody>
                                 {checkHour.map((hour, index) => {
                                   if (
-                                    format(fromUnixTime(hour.time), "MMMM do yyyy") ===
+                                    format(fromUnixTime(hour.dt), "MMMM do yyyy") ===
                                     format(fromUnixTime(item.dt), "MMMM do yyyy")
                                   ) {
                                     return (
-                                      <tr key={hour.time}>
+                                      <tr key={hour.dt}>
                                         <th scope="row">
-                                          {format(fromUnixTime(hour.time), "h a")}{" "}
+                                          {format(fromUnixTime(hour.dt), "h a")}{" "}
                                           <WeatherIcon condition={hour.icon} color="black" />
                                         </th>
                                         <td>{Math.round(hour.temperature)} °</td>
--- components/Weather/WeatherSlider/HourlySlider/HourlySlider.js
+++ components/Weather/WeatherSlider/HourlySlider/HourlySlider.js
@@ -65,9 +65,9 @@ const hourlySlider = props => {
                   ×
                 </button>
                 <h2 className={styles.date}>
-                  {format(new Date(), "eee") === format(fromUnixTime(item.time), "eee")
-                    ? format(fromUnixTime(item.time), "h a")
-                    : format(fromUnixTime(item.time), "eee h a")}
+                  {format(new Date(), "eee") === format(fromUnixTime(item.dt), "eee")
+                    ? format(fromUnixTime(item.dt), "h a")
+                    : format(fromUnixTime(item.dt), "eee h a")}
                 </h2>
                 <div className="row">
                   <div className="col-6 col-xs-6 col-sm-6 col-md-3">
@@ -110,7 +110,7 @@ const hourlySlider = props => {
                         <path d="M64.5 92.6c15.5 0 28-12.6 28-28s-12.6-28-28-28-28 12.6-28 28 12.5 28 28 28zm0-47.9c11 0 19.9 8.9 19.9 19.9 0 11-8.9 19.9-19.9 19.9s-19.9-8.9-19.9-19.9c0-11 8.9-19.9 19.9-19.9zM68.6 23.6V10.7c0-2.3-1.8-4.1-4.1-4.1s-4.1 1.8-4.1 4.1v12.9c0 2.3 1.8 4.1 4.1 4.1s4.1-1.8 4.1-4.1zM60.4 105.6v12.9c0 2.3 1.8 4.1 4.1 4.1s4.1-1.8 4.1-4.1v-12.9c0-2.3-1.8-4.1-4.1-4.1s-4.1 1.8-4.1 4.1zM96.4 38.5l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8 0l-9.1 9.1c-1.6 1.6-1.6 4.2 0 5.8.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2zM23.5 105.6c.8.8 1.8 1.2 2.9 1.2 1 0 2.1-.4 2.9-1.2l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8 0l-9.1 9.1c-1.6 1.6-1.6 4.2 0 5.8zM122.5 64.6c0-2.3-1.8-4.1-4.1-4.1h-12.9c-2.3 0-4.1 1.8-4.1 4.1s1.8 4.1 4.1 4.1h12.9c2.2 0 4.1-1.8 4.1-4.1zM10.6 68.7h12.9c2.3 0 4.1-1.8 4.1-4.1s-1.8-4.1-4.1-4.1H10.6c-2.3 0-4.1 1.8-4.1 4.1s1.9 4.1 4.1 4.1zM102.6 106.8c1 0 2.1-.4 2.9-1.2 1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8 0-1.6 1.6-1.6 4.2 0 5.8l9.1 9.1c.8.8 1.9 1.2 2.9 1.2zM38.4 38.5c1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8 0-1.6 1.6-1.6 4.2 0 5.8l9.1 9.1c.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2z" />
                       </svg>
                       <h4 className={styles["modal__item__title"]}>UV Index</h4>
-                      {item.uvIndex}
+                      {item.uvi}
                     </div>
                   </div>
                 </div>
@@ -130,9 +130,9 @@ const hourlySlider = props => {
                     <span>
                       {
                         Moon.phase(
-                          parseInt(format(fromUnixTime(item.time), "yyyy")),
-                          parseInt(format(fromUnixTime(item.time), "M")),
-                          parseInt(format(fromUnixTime(item.time), "d"))
+                          parseInt(format(fromUnixTime(item.dt), "yyyy")),
+                          parseInt(format(fromUnixTime(item.dt), "M")),
+                          parseInt(format(fromUnixTime(item.dt), "d"))
                         ).name
                       }
                     </span>
